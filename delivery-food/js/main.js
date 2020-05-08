411d8b4fba8e1dc0e5e08e7928e7ff210a5e8704
vsbdn <v.boldurean@gmail.com>
2020-05-08 19:39:53 +0100

    Added restaurants window functional

--- delivery-food/js/main.js
+++ delivery-food/js/main.js
@@ -1,15 +1,8 @@
+'use strict';
+
 const cartButton = document.querySelector("#cart-button");
 const modal = document.querySelector(".modal");
 const close = document.querySelector(".close");
-cartButton.addEventListener("click", toggleModal);
-close.addEventListener("click", toggleModal);
-
-function toggleModal() {
-  modal.classList.toggle("is-open");
-}
-
-//day 1
-
 const buttonAuth = document.querySelector(".button-auth");
 const modalAuth = document.querySelector(".modal-auth");
 const closeAuth = document.querySelector(".close-auth");
@@ -17,17 +10,24 @@ const logInForm = document.querySelector("#logInForm");
 const loginInput = document.querySelector("#login");
 const userName = document.querySelector(".user-name");
 const buttonOut = document.querySelector(".button-out");
+const cardsRestaurants = document.querySelector('.cards-restaurants');
+const containerPromo = document.querySelector(".container-promo");
+const restaurants = document.querySelector('.restaurants');
+const menu = document.querySelector('.menu');
+const logo = document.querySelector('.logo');
+const cardsMenu = document.querySelector('.cards-menu');
 
-let login = localStorage.getItem("gloDelivery");
+let login = localStorage.getItem('gloDelivery');
 
+function toggleModal() {
+  modal.classList.toggle("is-open");
+}
 
 function toggleModalAuth() {
-  loginInput.style.borderColor = "";
   modalAuth.classList.toggle("is-open");
 }
 
 function authorized() {
-  console.log("Authorized");
 
   function logOut() {
     login = null;
@@ -39,6 +39,8 @@ function authorized() {
     checkAuth();
   }
 
+  console.log('Авторизирован');
+
   userName.textContent = login;
 
   buttonAuth.style.display = "none";
@@ -49,18 +51,20 @@ function authorized() {
 }
 
 function notAuthorized() {
-  console.log("notAuthorized");
+  console.log('Не авторизирован');
 
   function logIn(event) {
     event.preventDefault();
-
-    if (loginInput.value.trim()) {
+    if (loginInput.value) {
+      loginInput.style.borderColor = '';
       login = loginInput.value;
+
       localStorage.setItem("gloDelivery", login);
+
       toggleModalAuth();
       buttonAuth.removeEventListener("click", toggleModalAuth);
       closeAuth.removeEventListener("click", toggleModalAuth);
-      logInForm.removeEventListener("submit", logIn)
+      logInForm.removeEventListener("submit", logIn);
       logInForm.reset();
       checkAuth();
     } else {
@@ -70,7 +74,7 @@ function notAuthorized() {
 
   buttonAuth.addEventListener("click", toggleModalAuth);
   closeAuth.addEventListener("click", toggleModalAuth);
-  logInForm.addEventListener("submit", logIn)
+  logInForm.addEventListener("submit", logIn);
 }
 
 function checkAuth() {
@@ -81,6 +85,90 @@ function checkAuth() {
   }
 }
 
+function createCardRestaurant() {
+  const card = `
+    <a class="card card-restaurant">
+      <img src="img/tanuki/preview.jpg" alt="image" class="card-image"/>
+      <div class="card-text">
+        <div class="card-heading">
+            <h3 class="card-title">Тануки</h3>
+            <span class="card-tag tag">60 мин</span>
+        </div>
+        <div class="card-info">
+          <div class="rating">
+              4.5
+          </div>
+          <div class="price">От 1 200 ₽</div>
+          <div class="category">Суши, роллы</div>
+        </div>
+      </div>
+    </a>`;
+
+  cardsRestaurants.insertAdjacentHTML('beforeend', card);
+}
+
+function createCardGood() {
+
+  const card = document.createElement('div');
+  card.className = 'card';
+
+  card.insertAdjacentHTML('beforeend', `
+    <img src="img/pizza-plus/pizza-classic.jpg" alt="image" class="card-image"/>
+    <div class="card-text">
+      <div class="card-heading">
+        <h3 class="card-title card-title-reg">Пицца Классика</h3>
+      </div>
+      <div class="card-info">
+        <div class="ingredients">Соус томатный, сыр «Моцарелла», сыр «Пармезан», ветчина, салями,
+            грибы.
+        </div>
+      </div>
+      <div class="card-buttons">
+        <button class="button button-primary button-add-cart">
+          <span class="button-card-text">В корзину</span>
+          <span class="button-cart-svg"></span>
+        </button>
+        <strong class="card-price-bold">510 ₽</strong>
+      </div>  
+    </div>
+  `);
+
+  cardsMenu.insertAdjacentElement("beforeend", card);
+
+}
+
+function openGoods(event) {
+
+  const target = event.target;
+
+  const restaurant = target.closest('.card-restaurant');
+
+  if (restaurant) {
+    cardsMenu.textContent = '';
+    containerPromo.classList.add('hide');
+    restaurants.classList.add('hide');
+    menu.classList.remove('hide');
+
+    createCardGood();
+    createCardGood();
+    createCardGood();
+  }
+}
+
+cartButton.addEventListener("click", toggleModal);
+
+close.addEventListener("click", toggleModal);
+
+cardsRestaurants.addEventListener('click', openGoods);
+
+logo.addEventListener('click', function () {
+  containerPromo.classList.remove('hide')
+  restaurants.classList.remove('hide')
+  menu.classList.add('hide')
+})
+
 checkAuth();
 
-
+createCardRestaurant();
+createCardRestaurant();
+createCardRestaurant();
